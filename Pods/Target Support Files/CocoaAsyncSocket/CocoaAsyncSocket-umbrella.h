fb39a9c8dc91d457603512143cfe5f83b1975551
zhangwh <zh.whong@gmail.com>
2016-09-02 15:09:08 +0800

    pod dependency CocoaAsyncSocket update to the version 7.5.0: Better IPv6 support

--- Pods/Target Support Files/CocoaAsyncSocket/CocoaAsyncSocket-umbrella.h
+++ Pods/Target Support Files/CocoaAsyncSocket/CocoaAsyncSocket-umbrella.h
@@ -2,8 +2,6 @@
 
 #import "GCDAsyncSocket.h"
 #import "GCDAsyncUdpSocket.h"
-#import "AsyncSocket.h"
-#import "AsyncUdpSocket.h"
 
 FOUNDATION_EXPORT double CocoaAsyncSocketVersionNumber;
 FOUNDATION_EXPORT const unsigned char CocoaAsyncSocketVersionString[];
