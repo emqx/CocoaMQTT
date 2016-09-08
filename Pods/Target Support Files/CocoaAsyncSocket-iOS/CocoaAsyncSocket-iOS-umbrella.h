e578e5567e1d89ee985628b03c82095e162c0e4a
zhangwh <zh.whong@gmail.com>
2016-09-08 11:10:51 +0800

    add Pods/

---
+++ Pods/Target Support Files/CocoaAsyncSocket-iOS/CocoaAsyncSocket-iOS-umbrella.h
@@ -0,0 +1,8 @@
+#import <UIKit/UIKit.h>
+
+#import "GCDAsyncSocket.h"
+#import "GCDAsyncUdpSocket.h"
+
+FOUNDATION_EXPORT double CocoaAsyncSocketVersionNumber;
+FOUNDATION_EXPORT const unsigned char CocoaAsyncSocketVersionString[];
+
---
+++ Pods/Target Support Files/MSWeakTimer-OSX/MSWeakTimer-OSX-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_MSWeakTimer_OSX : NSObject
+@end
+@implementation PodsDummy_MSWeakTimer_OSX
+@end
