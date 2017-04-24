1c75e75f0ab86088f8df136bdbdcdbd906b75d7d
Alex Yu <alexyu.dc@gmail.com>
2017-04-24 18:01:44 +0800

    Unit tests init.

--- CocoaMQTTTests/CocoaMQTTTests.swift
+++ CocoaMQTTTests/CocoaMQTTTests.swift
@@ -9,16 +9,41 @@
 import XCTest
 @testable import CocoaMQTT
 
-class CocoaMQTTTests: XCTestCase {
+let host = "localhost"
+let port: UInt16 = 1883
+let cilentID = "ClientForUnitTesting001"
+
+class CocoaMQTTTests: XCTestCase, CocoaMQTTDelegate {
+    
+    var readyExpectation: XCTestExpectation?
+    var mqtt: CocoaMQTT = CocoaMQTT(clientID: cilentID, host: host, port: port)
     
     override func setUp() {
         super.setUp()
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+        
+        // custom set
+        mqtt.delegate = self
+        mqtt.logLevel = .debug
+        mqtt.autoReconnect = true
+        mqtt.keepAlive = 60
+        mqtt.autoReconnectTimeInterval = 5
     }
     
     override func tearDown() {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
         super.tearDown()
+    }
+    
+    func testConnect() {
+        
+        readyExpectation = expectation(description: "Swift Expectations")
+        
+        mqtt.connect()
+        
+        
+        waitForExpectations(timeout: 5) { error in
+            XCTAssert(self.mqtt.connState == .connected)
+        }
     }
     
     func testExample() {
@@ -31,6 +56,43 @@ class CocoaMQTTTests: XCTestCase {
         self.measure {
             // Put the code you want to measure the time of here.
         }
+    }
+    
+    /// MQTT connected with server
+    func mqtt(_ mqtt: CocoaMQTT, didConnect host: String, port: Int) {
+    }
+    func mqtt(_ mqtt: CocoaMQTT, didConnectAck ack: CocoaMQTTConnAck) {
+        readyExpectation?.fulfill()
+    }
+    func mqtt(_ mqtt: CocoaMQTT, didPublishMessage message: CocoaMQTTMessage, id: UInt16) {
+        
+    }
+    func mqtt(_ mqtt: CocoaMQTT, didPublishAck id: UInt16) {
+        
+    }
+    func mqtt(_ mqtt: CocoaMQTT, didReceiveMessage message: CocoaMQTTMessage, id: UInt16 ) {
+        
+    }
+    func mqtt(_ mqtt: CocoaMQTT, didSubscribeTopic topic: String) {
+        
+    }
+    func mqtt(_ mqtt: CocoaMQTT, didUnsubscribeTopic topic: String) {
+        
+    }
+    func mqttDidPing(_ mqtt: CocoaMQTT) {
+        
+    }
+    func mqttDidReceivePong(_ mqtt: CocoaMQTT) {
+        
+    }
+    func mqttDidDisconnect(_ mqtt: CocoaMQTT, withError err: Error?) {
+        readyExpectation?.fulfill()
+    }
+    func mqtt(_ mqtt: CocoaMQTT, didReceive trust: SecTrust, completionHandler: @escaping (Bool) -> Void) {
+        
+    }
+    func mqtt(_ mqtt: CocoaMQTT, didPublishComplete id: UInt16) {
+        
     }
     
 }
--- Podfile
+++ Podfile
@@ -5,6 +5,11 @@ target 'CocoaMQTT iOS' do
     pod 'SwiftyTimer'
 end
 
+target 'CocoaMQTT iOSTests' do
+    pod 'CocoaAsyncSocket'
+    pod 'SwiftyTimer'
+end
+
 target 'CocoaMQTT OSX' do
     pod 'CocoaAsyncSocket'
     pod 'SwiftyTimer'
