c1dd763c6c9566ad155652c82c0f08f5f8528477
Alex Yu <alexyu.dc@gmail.com>
2017-04-25 14:50:17 +0800

    add unit test for connection, subscription, publish, etc (use XCTestExpectation)

--- CocoaMQTTTests/CocoaMQTTTests.swift
+++ CocoaMQTTTests/CocoaMQTTTests.swift
@@ -15,8 +15,17 @@ let cilentID = "ClientForUnitTesting001"
 
 class CocoaMQTTTests: XCTestCase, CocoaMQTTDelegate {
     
-    var readyExpectation: XCTestExpectation?
     var mqtt: CocoaMQTT = CocoaMQTT(clientID: cilentID, host: host, port: port)
+    
+    var connExp: XCTestExpectation?
+    var subExp: XCTestExpectation?
+    
+    var pubQos1Exp: XCTestExpectation?
+    var pubQos2Exp: XCTestExpectation?
+    
+    var res0Exp: XCTestExpectation?
+    var res1Exp: XCTestExpectation?
+    var res2Exp: XCTestExpectation?
     
     override func setUp() {
         super.setUp()
@@ -26,7 +35,7 @@ class CocoaMQTTTests: XCTestCase, CocoaMQTTDelegate {
         mqtt.logLevel = .debug
         mqtt.autoReconnect = true
         mqtt.keepAlive = 60
-        mqtt.autoReconnectTimeInterval = 5
+        mqtt.autoReconnectTimeInterval = 20
     }
     
     override func tearDown() {
@@ -35,46 +44,65 @@ class CocoaMQTTTests: XCTestCase, CocoaMQTTDelegate {
     }
     
     func testConnect() {
+        connExp = expectation(description: "connection")
+        subExp = expectation(description: "sub")
         
-        readyExpectation = expectation(description: "Swift Expectations")
+        pubQos1Exp = expectation(description: "pub_1")
+        pubQos2Exp = expectation(description: "pub_2")
+        
+        res0Exp = expectation(description: "res_0")
+        res1Exp = expectation(description: "res_1")
+        res2Exp = expectation(description: "res_2")
         
         mqtt.connect()
+        wait(for: [connExp!], timeout: 5)
         
+        mqtt.subscribe("animals")
+        wait(for: [subExp!], timeout: 5)
+
+
+        mqtt.publish("animals", withString: "0", qos: .qos0, retained: false, dup: false)
+        wait(for: [res0Exp!], timeout: 5)
+
+        mqtt.publish("animals", withString: "1", qos: .qos1, retained: false, dup: false)
+        wait(for: [pubQos1Exp!, res1Exp!], timeout: 5)
         
-        waitForExpectations(timeout: 5) { error in
-            XCTAssert(self.mqtt.connState == .connected)
-        }
+        mqtt.publish("animals", withString: "2", qos: .qos2, retained: false, dup: false)
+        wait(for: [pubQos2Exp!, res2Exp!], timeout: 5)
     }
     
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-    
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
-    }
+    //MARK: - CocoaMQTTDelegate
     
     /// MQTT connected with server
     func mqtt(_ mqtt: CocoaMQTT, didConnect host: String, port: Int) {
     }
     func mqtt(_ mqtt: CocoaMQTT, didConnectAck ack: CocoaMQTTConnAck) {
-        readyExpectation?.fulfill()
+        connExp?.fulfill()
     }
     func mqtt(_ mqtt: CocoaMQTT, didPublishMessage message: CocoaMQTTMessage, id: UInt16) {
         
     }
     func mqtt(_ mqtt: CocoaMQTT, didPublishAck id: UInt16) {
-        
+        pubQos1Exp?.fulfill()
     }
+    
+    func mqtt(_ mqtt: CocoaMQTT, didPublishComplete id: UInt16) {
+        pubQos2Exp?.fulfill()
+    }
+    
     func mqtt(_ mqtt: CocoaMQTT, didReceiveMessage message: CocoaMQTTMessage, id: UInt16 ) {
         
+        let string = message.string!
+        if string == "0" {
+            res0Exp?.fulfill()
+        } else if string == "1" {
+            res1Exp?.fulfill()
+        } else if string == "2" {
+            res2Exp?.fulfill()
+        }
     }
     func mqtt(_ mqtt: CocoaMQTT, didSubscribeTopic topic: String) {
-        
+        subExp?.fulfill()
     }
     func mqtt(_ mqtt: CocoaMQTT, didUnsubscribeTopic topic: String) {
         
@@ -86,13 +114,9 @@ class CocoaMQTTTests: XCTestCase, CocoaMQTTDelegate {
         
     }
     func mqttDidDisconnect(_ mqtt: CocoaMQTT, withError err: Error?) {
-        readyExpectation?.fulfill()
     }
+    
     func mqtt(_ mqtt: CocoaMQTT, didReceive trust: SecTrust, completionHandler: @escaping (Bool) -> Void) {
         
     }
-    func mqtt(_ mqtt: CocoaMQTT, didPublishComplete id: UInt16) {
-        
-    }
-    
 }
