ec06d178933b4c41193c7deec71342441f252f0e
Alex Yu <alexyu.dc@gmail.com>
2017-04-25 17:25:41 +0800

    add unit tests: auto reconn, long string publish

--- CocoaMQTTTests/CocoaMQTTTests.swift
+++ CocoaMQTTTests/CocoaMQTTTests.swift
@@ -10,12 +10,20 @@ import XCTest
 @testable import CocoaMQTT
 
 let host = "localhost"
+//let host = "q.emqtt.com"
 let port: UInt16 = 1883
-let cilentID = "ClientForUnitTesting001"
+let clientID = "ClientForUnitTesting-" + randomCode(length: 6)
+
+let timeout: TimeInterval = 5
+let keepAlive: UInt16 = 20
+let autoReconn: UInt16 = 5
+
+let topicToSub = "animals"
+let longString = longStringGen()
 
 class CocoaMQTTTests: XCTestCase, CocoaMQTTDelegate {
     
-    var mqtt: CocoaMQTT = CocoaMQTT(clientID: cilentID, host: host, port: port)
+    var mqtt: CocoaMQTT = CocoaMQTT(clientID: clientID, host: host, port: port)
     
     var connExp: XCTestExpectation?
     var subExp: XCTestExpectation?
@@ -29,17 +37,15 @@ class CocoaMQTTTests: XCTestCase, CocoaMQTTDelegate {
     
     override func setUp() {
         super.setUp()
-        
         // custom set
         mqtt.delegate = self
         mqtt.logLevel = .debug
         mqtt.autoReconnect = true
-        mqtt.keepAlive = 60
-        mqtt.autoReconnectTimeInterval = 20
+        mqtt.keepAlive = keepAlive
+        mqtt.autoReconnectTimeInterval = autoReconn
     }
     
     override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
         super.tearDown()
     }
     
@@ -55,20 +61,38 @@ class CocoaMQTTTests: XCTestCase, CocoaMQTTDelegate {
         res2Exp = expectation(description: "res_2")
         
         mqtt.connect()
-        wait(for: [connExp!], timeout: 5)
+        wait(for: [connExp!], timeout: timeout)
         
-        mqtt.subscribe("animals")
-        wait(for: [subExp!], timeout: 5)
-
-
-        mqtt.publish("animals", withString: "0", qos: .qos0, retained: false, dup: false)
-        wait(for: [res0Exp!], timeout: 5)
-
-        mqtt.publish("animals", withString: "1", qos: .qos1, retained: false, dup: false)
-        wait(for: [pubQos1Exp!, res1Exp!], timeout: 5)
+        if mqtt.connState != .connected {
+            XCTFail()
+        }
         
-        mqtt.publish("animals", withString: "2", qos: .qos2, retained: false, dup: false)
-        wait(for: [pubQos2Exp!, res2Exp!], timeout: 5)
+        mqtt.subscribe(topicToSub)
+        wait(for: [subExp!], timeout: timeout)
+
+        mqtt.publish(topicToSub, withString: "0", qos: .qos0, retained: false, dup: false)
+        wait(for: [res0Exp!], timeout: timeout)
+
+        mqtt.publish(topicToSub, withString: "1", qos: .qos1, retained: false, dup: false)
+        wait(for: [pubQos1Exp!, res1Exp!], timeout: timeout)
+        
+        mqtt.publish(topicToSub, withString: "2", qos: .qos2, retained: false, dup: false)
+        wait(for: [pubQos2Exp!, res2Exp!], timeout: timeout)
+        
+        pubQos1Exp = expectation(description: "pub_1")
+        res1Exp = expectation(description: "res_1")
+        mqtt.publish(topicToSub, withString: longString, qos: .qos1, retained: false, dup: false)
+        wait(for: [pubQos1Exp!, res1Exp!], timeout: timeout)
+    }
+    
+    func testAutoReconnect() {
+        connExp = expectation(description: "connection")
+        mqtt.connect()
+        wait(for: [connExp!], timeout: timeout)
+        
+        connExp = expectation(description: "connection")
+        mqtt.internal_disconnect()
+        wait(for: [connExp!], timeout: 10)
     }
     
     //MARK: - CocoaMQTTDelegate
@@ -76,12 +100,15 @@ class CocoaMQTTTests: XCTestCase, CocoaMQTTDelegate {
     /// MQTT connected with server
     func mqtt(_ mqtt: CocoaMQTT, didConnect host: String, port: Int) {
     }
+    
     func mqtt(_ mqtt: CocoaMQTT, didConnectAck ack: CocoaMQTTConnAck) {
         connExp?.fulfill()
     }
+    
     func mqtt(_ mqtt: CocoaMQTT, didPublishMessage message: CocoaMQTTMessage, id: UInt16) {
         
     }
+    
     func mqtt(_ mqtt: CocoaMQTT, didPublishAck id: UInt16) {
         pubQos1Exp?.fulfill()
     }
@@ -91,7 +118,6 @@ class CocoaMQTTTests: XCTestCase, CocoaMQTTDelegate {
     }
     
     func mqtt(_ mqtt: CocoaMQTT, didReceiveMessage message: CocoaMQTTMessage, id: UInt16 ) {
-        
         let string = message.string!
         if string == "0" {
             res0Exp?.fulfill()
@@ -99,11 +125,22 @@ class CocoaMQTTTests: XCTestCase, CocoaMQTTDelegate {
             res1Exp?.fulfill()
         } else if string == "2" {
             res2Exp?.fulfill()
+        } else if string == longString {
+            res1Exp?.fulfill()
+        } else {
+            
         }
     }
+    
     func mqtt(_ mqtt: CocoaMQTT, didSubscribeTopic topic: String) {
-        subExp?.fulfill()
+        if topic == topicToSub {
+            subExp?.fulfill()
+        } else {
+            XCTFail()
+        }
+        
     }
+    
     func mqtt(_ mqtt: CocoaMQTT, didUnsubscribeTopic topic: String) {
         
     }
@@ -119,4 +156,29 @@ class CocoaMQTTTests: XCTestCase, CocoaMQTTDelegate {
     func mqtt(_ mqtt: CocoaMQTT, didReceive trust: SecTrust, completionHandler: @escaping (Bool) -> Void) {
         
     }
+}
+
+// tools
+
+private func randomCode(length: Int) -> String {
+    let base62chars = [Character]("0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz".characters)
+    var code = ""
+    for _ in 0..<length {
+        let random = Int(arc4random_uniform(62))
+        code.append(base62chars[random])
+    }
+    return code
+}
+
+private func longStringGen() -> String {
+    var string = ""
+    let shijing = "燕燕于飞，差池其羽。之子于归，远送于野。瞻望弗及，泣涕如雨。\n" +
+                  "燕燕于飞，颉之颃之。之子于归，远于将之。瞻望弗及，伫立以泣。\n" +
+                  "燕燕于飞，下上其音。之子于归，远送于南。瞻望弗及，实劳我心。\n" +
+                  "仲氏任只，其心塞渊。终温且惠，淑慎其身。先君之思，以勗寡人。\n"
+    
+    for _ in 1...100 {
+        string.append(shijing)
+    }
+    return string
 }
