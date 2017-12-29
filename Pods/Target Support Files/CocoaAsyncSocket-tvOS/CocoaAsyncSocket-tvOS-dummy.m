374c197e997aa39e5a9b1c0e0ee1e56a9bf9171f
HeeeJianBo <heeejianbo@gmail.com>
2017-12-29 15:59:09 +0800

    Clean & Ignore unnecessary files

--- Pods/Target Support Files/CocoaAsyncSocket-tvOS/CocoaAsyncSocket-tvOS-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_CocoaAsyncSocket_tvOS : NSObject
-@end
-@implementation PodsDummy_CocoaAsyncSocket_tvOS
-@end
--- Pods/Target Support Files/SwiftyTimer-macOS/SwiftyTimer-macOS-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_SwiftyTimer_macOS : NSObject
-@end
-@implementation PodsDummy_SwiftyTimer_macOS
-@end
