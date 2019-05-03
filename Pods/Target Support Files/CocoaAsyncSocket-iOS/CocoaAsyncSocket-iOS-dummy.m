fcb492293c11e27e1044651ccfb5e843051b3471
HeeeJianBo <heeejianbo@gmail.com>
2019-05-03 17:20:51 +0800

    Compile works well

--- Pods/Target Support Files/CocoaAsyncSocket-iOS/CocoaAsyncSocket-iOS-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_CocoaAsyncSocket_iOS : NSObject
-@end
-@implementation PodsDummy_CocoaAsyncSocket_iOS
-@end
--- Pods/Target Support Files/Pods-CocoaMQTT tvOS/Pods-CocoaMQTT tvOS-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_CocoaMQTT_tvOS : NSObject
-@end
-@implementation PodsDummy_Pods_CocoaMQTT_tvOS
-@end
