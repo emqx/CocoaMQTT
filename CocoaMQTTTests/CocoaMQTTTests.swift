f265828a398ac8668023962c16fc68c6ca2f2a01
HeeeJianBo <heeejianbo@gmail.com>
2019-06-12 20:05:05 +0800

    Fix the deliver process safe error

--- CocoaMQTTTests/CocoaMQTTTests.swift
+++ CocoaMQTTTests/CocoaMQTTTests.swift
@@ -21,7 +21,7 @@ let autoReconn: UInt16 = 5
 let topicToSub = "animals"
 let longString = longStringGen()
 
-class CocoaMQTTTests: XCTestCase, CocoaMQTTDelegate {
+class CocoaMQTTTests: XCTestCase {
     
     var mqtt: CocoaMQTT = CocoaMQTT(clientID: clientID, host: host, port: port)
     
@@ -34,6 +34,11 @@ class CocoaMQTTTests: XCTestCase, CocoaMQTTDelegate {
     var res0Exp: XCTestExpectation?
     var res1Exp: XCTestExpectation?
     var res2Exp: XCTestExpectation?
+    
+
+    var maxMessageCount: UInt = 100
+    var multiCounter: UInt = 0
+    var multiPub: XCTestExpectation?
     
     override func setUp() {
         super.setUp()
@@ -94,6 +99,33 @@ class CocoaMQTTTests: XCTestCase, CocoaMQTTDelegate {
         mqtt.internal_disconnect()
         wait(for: [connExp!], timeout: 10)
     }
+   
+    func testProcessSafePub() {
+        connExp = expectation(description: "connection")
+        _ = mqtt.connect()
+        wait(for: [connExp!], timeout: timeout)
+        
+        // subscribe first
+        subExp = expectation(description: "sub")
+        mqtt.subscribe("test/#")
+        wait(for: [subExp!], timeout: timeout)
+        
+        mqtt.inflightWindowSize = 10
+        mqtt.messageQueueSize = maxMessageCount
+        
+        multiPub = expectation(description: "process_safe")
+        
+        let concurrentQueue = DispatchQueue(label: "tests.cocoamqtt.emqx", qos: .default, attributes: .concurrent)
+        for i in 0 ..< maxMessageCount {
+            concurrentQueue.async { [weak self] in
+                self?.mqtt.publish("test/\(i)", withString: "m", qos: .qos1)
+            }
+        }
+        wait(for: [multiPub!], timeout: timeout)
+    }
+}
+
+extension CocoaMQTTTests: CocoaMQTTDelegate {
     
     //MARK: - CocoaMQTTDelegate
     
@@ -127,13 +159,18 @@ class CocoaMQTTTests: XCTestCase, CocoaMQTTDelegate {
             res2Exp?.fulfill()
         } else if string == longString {
             res1Exp?.fulfill()
-        } else {
-            
+        } else if string == "m" {
+            multiCounter += 1
+            if multiCounter == maxMessageCount {
+                multiPub?.fulfill()
+            }
         }
     }
     
     func mqtt(_ mqtt: CocoaMQTT, didSubscribeTopic topics: [String]) {
         if topics.first! == topicToSub {
+            subExp?.fulfill()
+        } else if topics.first! == "test/#" {
             subExp?.fulfill()
         } else {
             XCTFail()
