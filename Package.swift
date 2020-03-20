fbcb880755d975022a1d09dcde846e0581661061
Cyrus Ingraham <cyrus.ingraham@jamf.com>
2020-03-20 09:07:53 -0500

    Fixed spacing in Package.swift

--- Package.swift
+++ Package.swift
@@ -15,9 +15,9 @@ let package = Package(
         .library(name: "CocoaMQTTWebSocket", targets: [ "CocoaMQTTWebSocket" ])
     ],
     dependencies: [
-			.package(url: "https://github.com/robbiehanson/CocoaAsyncSocket", from: "7.6.4"),
-            .package(url: "https://github.com/daltoniam/Starscream.git", from: "3.0.2"),
-		],
+        .package(url: "https://github.com/robbiehanson/CocoaAsyncSocket", from: "7.6.4"),
+        .package(url: "https://github.com/daltoniam/Starscream.git", from: "3.0.2"),
+    ],
     targets: [
         .target(name: "CocoaMQTT",
                 dependencies: [ "CocoaAsyncSocket" ],
