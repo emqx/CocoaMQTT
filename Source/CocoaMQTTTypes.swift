a9844ca9df9a2bdf5734df77f1f8978691e79f85
leeway <leeway1208@vip.qq.com>
2021-09-21 12:02:08 +0800

    add specific platform support for each target

--- Source/CocoaMQTTTypes.swift
+++ Source/CocoaMQTTTypes.swift
@@ -11,7 +11,6 @@ import Foundation
 
 /// Encode and Decode big-endian UInt16
 extension UInt16 {
-    
     /// Most Significant Byte (MSB)
     private var highByte: UInt8 {
         return UInt8( (self & 0xFF00) >> 8)
@@ -29,16 +28,19 @@ extension UInt16 {
 
 
 extension String {
-    
     /// String with two bytes length
     var bytesWithLength: [UInt8] {
         return UInt16(utf8.count).hlBytes + utf8
+    }
+    
+    var stringUTF8: String {
+        let data = self.data(using: .nonLossyASCII)
+        return String(data: data!, encoding: .utf8) ?? ""
     }
 }
 
 
 extension Bool {
-    
     /// Bool to bit of UInt8
     var bit: UInt8 {
         return self ? 1 : 0
@@ -51,7 +53,6 @@ extension Bool {
 }
 
 extension UInt8 {
-    
     /// Read a bit value
     func bitAt(_ offset: UInt8) -> UInt8 {
         return (self >> offset) & 0x01
@@ -60,15 +61,15 @@ extension UInt8 {
 
 
 public enum CocoaMQTTError: Error {
-    @available(OSX 10.15, iOS 13.0, watchOS 6.0, tvOS 13.0, *)
-    public enum FoundationConnection: Error {
-        case closed(URLSessionWebSocketTask.CloseCode)
-    }
-
     case invalidURL
     case readTimeout
     case writeTimeout
+    @available(OSX 10.15, iOS 13.0, watchOS 6.0, tvOS 13.0, *)
+    public enum FoundationConnection : Error {
+        case closed(URLSessionWebSocketTask.CloseCode)
+    }
 }
+
 
 extension Array where Element == UInt8 {
     var summary: String {
@@ -87,7 +88,6 @@ extension Array where Element == UInt8 {
 
 
 extension Data {
-
     var uint8: UInt8 {
         var number: UInt8 = 0
         self.copyBytes(to:&number, count: MemoryLayout<UInt8>.size)
