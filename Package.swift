bef088f4da8d76ee097c9c8ab546ebd1dd92864d
leeway <leeway1208@vip.qq.com>
2022-09-21 23:00:07 +0800

    fix: change package.swift url

--- Package.swift
+++ Package.swift
@@ -15,7 +15,7 @@ let package = Package(
         .library(name: "CocoaMQTTWebSocket", targets: [ "CocoaMQTTWebSocket" ])
     ],
     dependencies: [
-        .package(url: "https://github.com/robbiehanson/CocoaAsyncSocket", from: "7.6.4"),
+        .package(url: "https://github.com/leeway1208/CocoaAsyncSocket", from: "1.0.1"),
         .package(url: "https://github.com/daltoniam/Starscream.git", from: "3.1.1"),
     ],
     targets: [
