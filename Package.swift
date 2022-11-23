354938b60768d257b34e7e4451a33fd31148da6e
leeway <leeway1208@vip.qq.com>
2022-11-23 20:17:30 +0800

    fix: MqttCocoaAsyncSocket header file to public (#497)

--- Package.swift
+++ Package.swift
@@ -15,7 +15,7 @@ let package = Package(
         .library(name: "CocoaMQTTWebSocket", targets: [ "CocoaMQTTWebSocket" ])
     ],
     dependencies: [
-        .package(url: "https://github.com/leeway1208/MqttCocoaAsyncSocket", from: "1.0.5"),
+        .package(url: "https://github.com/leeway1208/MqttCocoaAsyncSocket", from: "1.0.8"),
         .package(url: "https://github.com/daltoniam/Starscream.git", from: "3.1.1"),
     ],
     targets: [
