b9e8d92eda85efc0a346a4c32e161d661f573069
Nicolas Anjoran <nicolas.anjoran@gmail.com>
2023-10-31 09:48:03 +0100

    Bugfix: concatenate the key and value instead of just sending the value. This was causing issues (could not be parsed by EMQX, which disconnected the client).

--- Source/MqttPublishProperties.swift
+++ Source/MqttPublishProperties.swift
@@ -77,9 +77,8 @@ public class MqttPublishProperties: NSObject {
         }
         //3.3.2.3.7 Property Length User Property
         if let userProperty = self.userProperty {
-            let dictValues = [String](userProperty.values)
-            for (value) in dictValues {
-                properties += getMQTTPropertyData(type: CocoaMQTTPropertyName.userProperty.rawValue, value: value.bytesWithLength)
+            for (key, value) in userProperty {
+                properties += getMQTTPropertyData(type: CocoaMQTTPropertyName.userProperty.rawValue, value: key.bytesWithLength + value.bytesWithLength)
             }
         }
         //3.3.2.3.8 Subscription Identifier
