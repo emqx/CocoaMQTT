97f330c706bb053232a9e185eb2431fcd699dbed
appleApps-art <appleapps.github@gmail.com>
2025-09-12 09:57:40 +0300

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -14,7 +14,7 @@ let package = Package(
     ],
     dependencies: [
         // Starscream останній з master-гілки (це v4 API)
-        .package(url: "https://github.com/daltoniam/Starscream.git", branch: "master"),
+        .package(url: "https://github.com/daltoniam/Starscream.git", .branch("master")),
         .package(url: "https://github.com/leeway1208/MqttCocoaAsyncSocket", from: "1.0.8"),
     ],
     targets: [
